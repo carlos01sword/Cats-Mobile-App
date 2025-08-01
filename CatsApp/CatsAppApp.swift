fee3c5b5656312ebe128324bf0505693b79fac99
carlos01amc <carlos01amc@gmail.com>
2025-08-01 23:44:10 +0100

    Initial Commit

---
+++ CatsApp/CatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAppApp.swift
+//  CatsApp
+//
+//  Created by Carlos Costa on 01/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct CatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatsAppTests/CatsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAppTests.swift
+//  CatsAppTests
+//
+//  Created by Carlos Costa on 01/08/2025.
+//
+
+import Testing
+@testable import CatsApp
+
+struct CatsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
