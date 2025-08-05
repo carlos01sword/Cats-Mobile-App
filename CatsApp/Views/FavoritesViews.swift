f876ca6e51e8d84bbf3ba362140fa7f8a13707f0
carlos01amc <carlos01amc@gmail.com>
2025-08-05 14:09:03 +0100

    chore: implement basic FavoritesViewto show name of the selected favorites breed

--- CatsApp/Views/FavoritesViews.swift
+++ CatsApp/Views/FavoritesViews.swift
@@ -9,10 +9,19 @@ import SwiftUI
 import SwiftData
 
 struct FavoritesView: View {
-    var body: some View {
-        NavigationView {
-            
+    @Environment(\.modelContext) private var context
+    @Query(
+        filter: #Predicate { (breed: CatBreed) in
+          breed.isFavorite
         }
-        .navigationTitle("Favorites")
+      ) private var favoriteBreeds: [CatBreed]
+    
+    var body: some View {
+        NavigationStack {
+            List(favoriteBreeds) { breed in
+                Text(breed.name)
+            }
+            .navigationTitle("Favorites")
+        }
     }
 }
