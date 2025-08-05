446d309e29c4b3347fe0fbba2e55e6effe761d39
carlos01amc <carlos01amc@gmail.com>
2025-08-05 14:34:29 +0100

    refactor: update FavortiesView to match CatDataView styling

--- CatsApp/Views/FavoritesViews.swift
+++ CatsApp/Views/FavoritesViews.swift
@@ -12,14 +12,36 @@ struct FavoritesView: View {
     @Environment(\.modelContext) private var context
     @Query(
         filter: #Predicate { (breed: CatBreed) in
-          breed.isFavorite
+            breed.isFavorite
         }
-      ) private var favoriteBreeds: [CatBreed]
+    ) private var favoriteBreeds: [CatBreed]
     
     var body: some View {
         NavigationStack {
             List(favoriteBreeds) { breed in
-                Text(breed.name)
+                HStack(spacing: 16) {
+                    if let imageUrl = breed.referenceImageUrl,
+                       let url = URL(string: imageUrl) {
+                        AsyncImage(url: url) { image in
+                            image.resizable().scaledToFill()
+                        } placeholder: {
+                            Color.gray.opacity(1)
+                        }
+                        .frame(width: 60, height: 60)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                    } else {
+                        Color.gray.opacity(0.1)
+                            .frame(width: 60, height: 60)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    }
+                    
+                    Text(breed.name)
+                        .font(.headline)
+                    
+                    Spacer()
+                }
+                .padding(.vertical, 5)
+                
             }
             .navigationTitle("Favorites")
         }
