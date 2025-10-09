c8c2094894644cef6d2ae231e507eac24fa1a54f
Carlos Costa <ca.costa@swordhealth.com>
2025-10-09 17:40:57 +0100

    refactor: BreedsViewModel to use SearchService - Add SearchService

---
+++ CatsApp/Data/Services/SearchService.swift
@@ -0,0 +1,19 @@
+//
+//  SearchService.swift
+//  CatsApp
+//
+//  Created by Carlos Costa on 09/10/2025.
+//
+
+import Foundation
+
+protocol SearchServiceProtocol {
+    func searchBreeds(query: String, in breeds: [CatBreed]) -> [CatBreed]
+}
+
+struct SearchService: SearchServiceProtocol {
+    func searchBreeds(query: String, in breeds: [CatBreed]) -> [CatBreed] {
+        guard !query.isEmpty else { return breeds }
+        return breeds.filter { $0.name.lowercased().contains(query.lowercased()) }
+    }
+}
